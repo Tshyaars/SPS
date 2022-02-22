df5187858babc667ff6c5375d4f2145ce1456f4e
Tshyaars Aestrelis <tshyaars.aestrelis@gmail.com>
2022-02-22 15:19:29 +0100

    +doing its job

--- PFC.js
+++ PFC.js
@@ -7,8 +7,9 @@ const coupJ1 = document.querySelector('.coupJ1');
 const actions = document.querySelectorAll('input');
 const scorePC = document.querySelector('#PC');
 const scoreJ1 = document.querySelector('#J1');
+const msgs = document.querySelector(".vide");
+const rejouer = document.querySelector(".replay");
 let hasWon = false;
-
 function joueCoupAleatoire() {
     return parseInt(Math.random() * coups.length);
 }
@@ -28,12 +29,41 @@ function round(J1,PC) {
     }
     return hasWon;
 }
+function checkIfWin() {
+    if (scoreJ1.innerHTML == 3 || scorePC.innerHTML == 3) {
+        hasWon=true;
+        msgs.innerText = scoreJ1.innerHTML == 3 ? "J1" : "PC" + " sort vainqueur de cette partie !";
+        rejouer.classList.remove("hidden");
+        rejouer.addEventListener("click",()=>{
+            scoreJ1.innerHTML = 0;
+            scorePC.innerHTML = 0;
+            hasWon = false;
+            msgs.innerText="";
+            coupJ1.innerText = "Thinking...";
+            coupPC.innerText = "Thinking...";
+            rejouer.classList.add("hidden");
+        });
+    }
+}
 actions.forEach(action => {
     action.addEventListener("click", ()=>{
-        coupJ1.innerText = action.value;
-        coupPC.innerText = coups[joueCoupAleatoire()]
-        if (coupJ1.innerText != coupPC.innerText) {
-            round(coupJ1.innerText,coupPC.innerText)?scoreJ1.innerHTML = parseInt(scoreJ1.innerHTML)+1:scorePC.innerHTML = parseInt(scorePC.innerHTML)+1;
+        if (!hasWon) {
+            coupJ1.innerText = action.value;
+            coupPC.innerText = coups[joueCoupAleatoire()]
+            if (coupJ1.innerText != coupPC.innerText) {
+                round(coupJ1.innerText,coupPC.innerText);
+                if (hasWon) {
+                    scoreJ1.innerHTML = parseInt(scoreJ1.innerHTML)+1;
+                    msgs.innerText = "Point pour J1";
+                }else{
+                    scorePC.innerHTML = parseInt(scorePC.innerHTML)+1;
+                    msgs.innerText = "Point pour PC";
+                }
+                hasWon=false;
+            }else{
+                msgs.innerText = "ex-aequo";
+            }
+            checkIfWin()
         }
     });
 });
